e4885a2fda60d55ecac7cb4c36fe1aab2f3a8fbd
raoole <espinaraul@gmail.com>
2022-11-06 12:47:23 -0400

    borrando archivos redundantes

--- dist/__test__/example.spec.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-describe("My frist test suite", () => {
-    it("My Firts test case", () => {
-        expect(true).toBe(true);
-    });
-});
-//# sourceMappingURL=example.spec.js.map
