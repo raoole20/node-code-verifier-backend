27f3f743d8fe31e242715e761f3f09ad693e7918
raoole <espinaraul@gmail.com>
2022-11-14 14:16:55 -0400

    add configuration tsoa

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,7 @@
 import express, { Express, Request, Response } from "express"
-
-
-// Security
 import cors from 'cors'
 import helmet from 'helmet'
+import swaggerUI from 'swagger-ui-express'
 
 // TODO https
 // TODO mongo connection
@@ -13,19 +11,29 @@ import routes from '../routes'
 
 const server: Express = express()
 
+// * Swagger Config and route
+server.use('/docs', swaggerUI.serve, swaggerUI.setup(undefined, {
+    swaggerOptions: {
+        url: '/swagger.json',
+        explorer: true
+    }
+}))
+
 // Define Server to use /api and use rootRouter from index.ts in rootes
 server.use('/api/', routes)
 
 // Static Server
 server.use( express.static('public') )
-server.use(helmet())
-server.use(cors())
+
+server.use( helmet() )
+server.use( cors() )
 
 // content type 
 server.use(express.urlencoded({
     extended: true,
     limit: '50mb'
 }))
+
 server.use( express.json({ limit: '50mb'}) )
 
 // redirecciones
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,28 @@
-const path = require('path')
-const nodeExternals = require('webpack-node-externals')
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        'index': './index.ts',
-    },
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: '[name].js',
-        publicPath: '/'
-    },
-    target: 'node',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: [
-            '.tsx', '.ts', '.js'
-        ],
-    },
-    externals: [nodeExternals()]
-}
+  mode: "development",
+  entry: {
+    index: "./index.ts",
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "[name].js",
+    publicPath: "/",
+  },
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  externals: [nodeExternals()],
+};
